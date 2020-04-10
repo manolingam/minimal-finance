2e990763686f32a4aee7f98dc166ef751fcdf71b
Manolingam <manolingam1996@gmail.com>
2020-04-10 18:23:48 +0530

    UI updates

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -4,7 +4,7 @@ import Skeleton from '@material-ui/lab/Skeleton';
 import Tooltip from '@material-ui/core/Tooltip';
 import { withStyles } from '@material-ui/core/styles';
 
-import Eth from '../../assets/eth.svg';
+import Eth from '../../assets/ether.png';
 
 import './styles.css';
 
@@ -48,68 +48,70 @@ class Stats extends React.Component {
 	render() {
 		return (
 			<div className='stats'>
-				<img
-					className='animated flipInY infinite delay-3s'
-					src={Eth}
-					alt='floating'
-				></img>
-
 				{this.props.appLoaded ? (
-					<LightTooltip
-						placement='right'
-						title='Your supplied ETH currently earning interest (updated every 15 secs)'
-						arrow
-					>
-						<p id='underlyingBalance'>
-							{this.state.balanceOfUnderlying}
-							<br></br>
-							<span style={{ fontSize: '0.5em' }}>
-								{' '}
-								(Supplied Balance)
-							</span>
-						</p>
-					</LightTooltip>
+					<img src={Eth} alt='floating'></img>
 				) : (
-					<Skeleton animation='wave' width={350} height={50} />
+					<Skeleton variant='rect' width={270} height={130} />
 				)}
 
-				{this.props.appLoaded ? (
-					<LightTooltip
-						placement='left'
-						title={`Your cToken Balance. Each cToken value increases as you earn interest. 1 cETH = ${this.props.exchangeRate} ETH.`}
-						arrow
-					>
-						<p id='cEthBalance'>
-							{this.props.cEthBalance}
-							<br></br>
-							<span style={{ fontSize: '0.5em' }}>
-								{' '}
-								(cEth Balance)
-							</span>
-						</p>
-					</LightTooltip>
-				) : (
-					<Skeleton animation='wave' width={350} height={50} />
-				)}
+				<div className='stats-sub'>
+					{this.props.appLoaded ? (
+						<LightTooltip
+							placement='right'
+							title='Your supplied ETH currently earning interest (updated every 15 secs)'
+							arrow
+						>
+							<p id='underlyingBalance'>
+								{this.state.balanceOfUnderlying}
+								<br></br>
+								<span style={{ fontSize: '0.5em' }}>
+									{' '}
+									(Supplied Balance)
+								</span>
+							</p>
+						</LightTooltip>
+					) : (
+						<Skeleton animation='wave' width={350} height={50} />
+					)}
 
-				{this.props.appLoaded ? (
-					<LightTooltip
-						placement='right'
-						title='Your pending loan value in ETH that needs to be repaid.'
-						arrow
-					>
-						<p id='borrowBalance'>
-							{this.props.borrowBalanceInEth}
-							<br></br>
-							<span style={{ fontSize: '0.5em' }}>
-								{' '}
-								(Borrow Balance)
-							</span>
-						</p>
-					</LightTooltip>
-				) : (
-					<Skeleton animation='wave' width={350} height={50} />
-				)}
+					{this.props.appLoaded ? (
+						<LightTooltip
+							placement='left'
+							title={`Your cToken Balance. Each cToken value increases as you earn interest. 1 cETH = ${this.props.exchangeRate} ETH.`}
+							arrow
+						>
+							<p id='cEthBalance'>
+								{this.props.cEthBalance}
+								<br></br>
+								<span style={{ fontSize: '0.5em' }}>
+									{' '}
+									(cEth Balance)
+								</span>
+							</p>
+						</LightTooltip>
+					) : (
+						<Skeleton animation='wave' width={350} height={50} />
+					)}
+
+					{this.props.appLoaded ? (
+						<LightTooltip
+							placement='right'
+							title='Your pending loan value in ETH that needs to be repaid.'
+							arrow
+						>
+							<p id='borrowBalance'>
+								{this.props.borrowBalanceInEth}
+								<br></br>
+								<span style={{ fontSize: '0.5em' }}>
+									{' '}
+									(Borrow Balance)
+								</span>
+							</p>
+						</LightTooltip>
+					) : (
+						<Skeleton animation='wave' width={350} height={50} />
+					)}
+				</div>
 			</div>
 		);
 	}
